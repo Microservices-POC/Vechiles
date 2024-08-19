ff19e8f64427cdb549f03a64ff4d76a09ffcec3f
Uzair Anwar <uzairch688@gmail.com>
2024-08-19 06:39:35 -0500

    Complete Vechiles service

---
+++ Controllers/VehicleController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vehicles.Data.Models;
+using Vehicles.Data.Repository;
+
+namespace Vehicles.Controller
+{
+    [Route("api/vehicles")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        private readonly IVehicleRepository _vehicleRepository;
+
+        public VehicleController(IVehicleRepository vehicleRepository)
+        {
+            _vehicleRepository = vehicleRepository;
+        }
+
+        [HttpGet("customervehicles/{customerId}")]
+        public ActionResult<IEnumerable<Vehicle>> GetVehiclesByCustomerId(int customerId)
+        {
+            try
+            {
+                var vehicles = _vehicleRepository.GetVehiclesByCustomerId(customerId);
+                if (!vehicles.Any())
+                {
+                    return NotFound($"No vehicles found for customer with ID {customerId}.");
+                }
+                return Ok(vehicles);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred while retrieving vehicles: {ex.Message}");
+            }
+        }
+
+    }
+}
